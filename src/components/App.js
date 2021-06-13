6f059b166039e04961566b568ade78adf836f5ef
Mohamed Afifi <mohamed.afifi@hotmail.com>
2021-06-13 14:04:39 +0200

    build Dashboard and pollPreview representionalcomponent

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleReceiveData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import NavBar from "./NavBar";
+
+import "./app.scss";
 class App extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
@@ -10,6 +13,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div className="nav">
+          <NavBar />
+        </div>
+
         <Dashboard />
       </div>
     );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PollPreview from "./PollPreview";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
     return (
       <div>
-        <div> nav</div>
+        <div className="dashboard">
+          <div className="title">
+            <h2>Unanswered Questions</h2>
+            <h2>Answered Questions</h2>
+          </div>
+          <div className="polls">
+            <PollPreview />
+            <PollPreview />
+            <PollPreview />
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+export default connect()(Dashboard);
